d8ffd4dcb758c79d382cbe9ee98198addef3ebda
ahmednoorani <ahmednoorani258@gmail.com>
2025-03-17 13:29:28 -0700

    game update

--- python-operators/tasks.py
+++ python-operators/tasks.py
@@ -19,14 +19,15 @@
 # ____________________________________
 
 # def double_sum_checker(arr,target):
+    
 #     for i in range(len(arr)):
 #         for j in range(i+1,len(arr)):
 #             if arr[i]+arr[j] == target:
-#                 return f"{arr[i]} + {arr[j]} = {target}"
+#                 return [i,j]
 #     return False
 
-# print(double_sum_checker([1,2,3,4,5],9))
-# print(double_sum_checker([1,2,5,4,5],9))
+# print(double_sum_checker([1,2,3,4,5],9))#[3,4]
+# print(double_sum_checker([1,2,5,4,5],9))#[2,3]
 
 # ____________________________________
 # ____________________________________
