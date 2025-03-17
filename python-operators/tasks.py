9245e3ad33b3480cf70ccab4c0ce33ab883567e2
ahmednoorani <ahmednoorani258@gmail.com>
2025-03-17 10:34:51 -0700

    update format

--- python-operators/tasks.py
+++ python-operators/tasks.py
@@ -1,17 +1,37 @@
+# ____________________________________
+# ____________________________________
 
-def palindromecheck(word):
-    return word == word[::-1]
+# Task 1 
+# ____________________________________
+# ____________________________________
 
-print(palindromecheck("racecar"))
-print(palindromecheck("121"))
+# def palindromecheck(word):
+#     return word == word[::-1]
+
+# print(palindromecheck("racecar"))
+# print(palindromecheck("121"))
+
+# ____________________________________
+# ____________________________________
     
-def double_sum_checker(arr,target):
-    for i in range(len(arr)):
-        for j in range(i+1,len(arr)):
-            if arr[i]+arr[j] == target:
-                return f"{arr[i]} + {arr[j]} = {target}"
-    return False
+# Task 2
+# ____________________________________
+# ____________________________________
 
-print(double_sum_checker([1,2,3,4,5],9))
-print(double_sum_checker([1,2,5,4,5],9))
+# def double_sum_checker(arr,target):
+#     for i in range(len(arr)):
+#         for j in range(i+1,len(arr)):
+#             if arr[i]+arr[j] == target:
+#                 return f"{arr[i]} + {arr[j]} = {target}"
+#     return False
+
+# print(double_sum_checker([1,2,3,4,5],9))
+# print(double_sum_checker([1,2,5,4,5],9))
+
+# ____________________________________
+# ____________________________________
+    
+# Task 3
+# ____________________________________
+# ____________________________________
     
